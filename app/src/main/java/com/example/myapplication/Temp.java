1e550b6d0cb5c68f35caeebddd513fa9e6f9da7b
leejungbin323 <leejungbin323@naver.com>
2020-08-11 11:44:04 +0900

    바텀바

--- app/src/main/java/com/example/myapplication/Temp.java
+++ app/src/main/java/com/example/myapplication/Temp.java
@@ -8,9 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Weather extends Fragment {
-
-    public Weather() {
+public class Temp extends Fragment {
+    public Temp() {
         // Required empty public constructor
     }
 
@@ -23,6 +22,6 @@ public class Weather extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        return inflater.inflate(R.layout.fragment_temp, container, false);
     }
 }
